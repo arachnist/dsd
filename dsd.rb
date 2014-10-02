ea76a20c43d14b7a14059a10757dcf9afb78df65
Robert Gerus <arachnist@i.am-a.cat>
2014-10-02 19:21:41 +0200

    Make it actually do something.

--- dsd.rb
+++ dsd.rb
@@ -2,6 +2,13 @@
 
 require 'eventmachine'
 require 'date'
+require 'ffi'
+
+module Xname
+    extend FFI::Library
+    ffi_lib 'xname'
+    attach_function :xname, [ :string ], :int
+end
 
 $a = []
 
@@ -9,5 +16,5 @@ EM.run do
     EM.add_periodic_timer(1) { $a[0] = DateTime.now.to_s }
     EM.add_periodic_timer(5) { $a[1] = File.read("/proc/loadavg").split[0..2].join " " }
 
-    EM.add_periodic_timer(0.1) { puts $a.join " " }
+    EM.add_periodic_timer(0.1) { Xname.xname $a.join " " }
 end
